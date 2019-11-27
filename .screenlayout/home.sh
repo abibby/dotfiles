5dd819132343d27e788fd47c88992f1b7906ba46
Adam Bibby <adam_bibby@hotmail.com>
2019-11-27 09:03:11 -0500

    dotfile updates on Wed Nov 27 09:03:11 EST 2019

--- .screenlayout/home.sh
+++ .screenlayout/home.sh
@@ -2,6 +2,6 @@
 xrandr --output DVI-I-0 --off \
        --output DVI-I-1 --off \
        --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal \
-       --output DP-0 --mode 1920x1080 --pos 4480x0 --rotate normal \
+       --output DP-0 --mode 1920x1080 --scale 1.5x1.5 --pos 4480x0 --rotate normal \
        --output DP-1 --off \
        --output DVI-D-0 --primary --mode 2560x1440 --pos 1920x0 --rotate normal
